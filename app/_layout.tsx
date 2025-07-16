ea695b27b11604105011383148cbf005a07aed5a
William <william.weng@student.tdsb.on.ca>
2025-07-16 16:42:50 -0400

    Added tabs, home and profile tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { Platform } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile'
+        }}
+      />
+
+    </Tabs>);
 }
