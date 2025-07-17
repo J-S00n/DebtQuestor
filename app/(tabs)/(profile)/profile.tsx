80e05b21a0822784154ea8f3e2ebb4ab854da26a
William <william.weng@student.tdsb.on.ca>
2025-07-17 18:28:04 -0400

    Added profile inputs and dropdown menus

--- app/(tabs)/(profile)/profile.tsx
+++ app/(tabs)/(profile)/profile.tsx
@@ -1,21 +1,85 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
 import Button from "@/components/Button";
 import { useRouter } from 'expo-router';
+import { useState } from "react";
+import Dropdown from "@/components/Dropdown";
 
 export default function Profile() {
   const router = useRouter();
-  return (
-    <View style={styles.container}>
-      <Button label = "User Settings" onPress={() => router.navigate('/userSettings')}/>
-      <Text style={styles.text}>Profile Screen</Text>
-    </View>
+  const [name, onChangeName] = useState("Name");
+  const [age, onChangeAge] = useState("Age");
+  const [income, onChangeIncome] = useState('0');
 
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        {/* name */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Name:</Text>
+          <TextInput
+            onChange={() => onChangeName}
+            value={name}
+            style={styles.input}
+          />
+        </View>
+        {/* age */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Age:</Text>
+          <TextInput
+            onChange={() => onChangeAge}
+            value={age}
+            style={styles.input}
+          />
+        </View>
+
+        {/* Program */}
+        <View style={styles.row}>
+          <Text style={styles.text}>University:</Text>
+          <Dropdown />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>Program:</Text>
+          <Dropdown />
+        </View>
+
+        {/* rent */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Rent:</Text>
+          <Dropdown />
+        </View>
+
+        {/* income */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Income:</Text>
+          <TextInput
+            onChange={() => onChangeIncome}
+            value={income}
+            style={styles.input}
+          />
+        </View>
+
+        {/* projected costs */}
+        <View style={styles.row}>
+          <Text style={styles.text}>Projected Costs for ___:</Text>
+          <TextInput
+            readOnly
+            style={styles.costs}
+          />
+        </View>
+        <Button label="User Settings" onPress={() => router.navigate('/userSettings')} />
+
+        {/* temporary filler block for scrolling; REMOVE LATER */}
+        <View style={styles.filler}></View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  filler: {
+    padding: 200
+  },
   container: {
-    flex: 1,
     justifyContent: "center",
     backgroundColor: "#25292e",
     alignItems: "center",
@@ -24,5 +88,26 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#fff",
     fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  costs: {
+    height: 40,
+    width: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
   },
 });
