abb2106dc0670403d8abc14dff1b4c33193e3fc4
J-S00n <jason.chen20@student.tdsb.on.ca>
2025-07-17 15:56:40 -0400

    Added styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,28 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 type Props = {
   label: string;
+  theme?: 'primary';
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === 'primary') {
+    return (
+      <View
+        style={[styles.buttonContainer,
+        { borderWidth: 1, borderColor: '#007bff', borderRadius: 1 },
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#007bff' }]}
+          onPress={() => alert('Navigating to your budget.')}>
+          <FontAwesome name="dollar" size={24} color="#fff" />
+          <Text style={[styles.buttonLabel, { color: '#2529e' }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
