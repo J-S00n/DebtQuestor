8182380bedab82683cc861afeed3349e463be9a2
William <william.weng@student.tdsb.on.ca>
2025-07-18 17:07:22 -0400

    added sign up inputs

--- app/signUp.tsx
+++ app/signUp.tsx
@@ -1,11 +1,16 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 import Button from "@/components/Button";
 import { useRouter } from 'expo-router';
 import { CommonActions, useNavigation } from '@react-navigation/native';
+import { useState } from "react";
 
 export default function signUp() {
     const router = useRouter();
     const navigation = useNavigation();
+    const [user, setUser] = useState("User");
+    const [pass, setPass] = useState("Password");
+    const [name, setName] = useState("Name");
+    const [age, setAge] = useState("Age");
     const resetToHome = () => {
         navigation.dispatch(
             // common actions deletes everything currently in the stack (index, calc, goal)
@@ -21,7 +26,42 @@ export default function signUp() {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Ready to get started? SIGN UP TODAY!</Text>
-            {/* dismiss to gets rid of the log in slideshow and makes it so you cannot go backwards */}
+            <View style={styles.row}>
+                <Text style={styles.text}>Name:</Text>
+                <TextInput
+                    onChange={() => setName}
+                    value={name}
+                    style={styles.input}
+                />
+            </View>
+
+            <View style={styles.row}>
+                <Text style={styles.text}>Age:</Text>
+                <TextInput
+                    onChange={() => setAge}
+                    value={age}
+                    style={styles.input}
+                />
+            </View>
+
+            <View style={styles.row}>
+                <Text style={styles.text}>Username:</Text>
+                <TextInput
+                    onChange={() => setUser}
+                    value={user}
+                    style={styles.input}
+                />
+            </View>
+
+            <View style={styles.row}>
+                <Text style={styles.text}>Password:</Text>
+                <TextInput
+                    onChange={() => setPass}
+                    value={pass}
+                    style={styles.input}
+                />
+            </View>
+
             <Button label="Go to HOME page" onPress={() => resetToHome()} />
             <View style={styles.row}>
                 <Text style={styles.text}>Not a new user? Click here to</Text>
@@ -53,5 +93,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 10,
-    }
+    },
+    input: {
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: "white",
+    },
 });
